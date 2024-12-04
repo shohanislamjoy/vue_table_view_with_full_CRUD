2afb61a181834a42c5fef80d55dbc9543ff7636b
Shohan <shohanislam60@gmail.com>
2024-12-04 12:21:45 +0600

    added axios and simple table added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
 
+import table_view from './components/table_view.vue';
+
 </script>
 
 <template>
+  <table_view foo="bar" />
+
+
+  
   <h1 class="font-bold text-center"> yooo</h1>
 </template>
 
